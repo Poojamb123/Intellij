c845780aa5a556a476549960927aedfddafe9642
Poojamb123 <144606718+Poojamb123@users.noreply.github.com>
2026-06-03 12:14:49 +0530

    1st commit

---
+++ src/Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter string1:");
+        String str1 = sc.nextLine();
+        System.out.println("enter string2:");
+        String str2 = sc.nextLine();
+        char c1[] = str1.toCharArray();
+            char c2[] = str2.toCharArray();
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+
+        if(Arrays.equals(c1,c2)){
+            System.out.println("anagram");
+        }else{
+            System.out.println("not anagram");
+        }
+    }
+    }
+
+
