f3c3b7e6b2a14cc4705106546b97bc24c7f23c13
pooja <pooja@ip-172-31-0-226.ap-southeast-2.compute.internal>
2026-06-06 08:25:56 +0000

    updated Doll.java

--- src/Doll.java
+++ src/Doll.java
@@ -1,6 +1,8 @@
 public class Doll {
     public static void main(String[] args){
-        System.out.print("wellcome to devops");
+        System.out.println("wellcome to devops");
+	System.out.print("hi pooja");
+
     }
 
 
